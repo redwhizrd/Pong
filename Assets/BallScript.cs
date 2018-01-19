3bbd875e58f057bd786d413b30379c9f0dabea10
John <johnparsons3.jp@gmail.com>
2018-01-18 21:53:42 -0500

    Functionalish

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -3,14 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BallScript : MonoBehaviour {
-    
-	// Use this for initialization
-	void Start () {
-	    	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Rigidbody2D rb;
+    private Vector2 inDirection;
+    private float speed = .1f;
+    public CircleCollider2D cc2d;
+    // Use this for initialization
+
+
+    void Start () {
+        inDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        transform.Translate(inDirection * speed, Space.World);
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        var contactPoint = col.contacts[0].point;
+        Vector2 ballLocation = transform.position;
+        var inNormal = (ballLocation - contactPoint).normalized;
+        inDirection = Vector2.Reflect(inDirection, inNormal);
+        
+    }
+
+  
 }
---
+++ Assets/PaddleScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+    public bool isFirst = false;
+    // Use this for initialization
+    AudioSource asss;
+    void Start () {
+
+        asss = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (isFirst)
+        {
+            Vector3 position = this.transform.position;
+           
+            
+            if (Input.GetKey(KeyCode.W))
+                position.y+=.1f;
+            if (Input.GetKey(KeyCode.S))
+                position.y-=.1f;
+
+            this.transform.position = position;
+        }
+        else
+        {
+            Vector3 position = this.transform.position;
+            if (Input.GetKey(KeyCode.UpArrow))
+                position.y+=.1f;
+            if (Input.GetKey(KeyCode.DownArrow))
+                position.y-=.1f;
+
+            this.transform.position = position;
+        }
+	}
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        asss.Play();
+        if (isFirst)
+        {
+        }
+        else
+        {
+            
+        }
+    }
+
+}
